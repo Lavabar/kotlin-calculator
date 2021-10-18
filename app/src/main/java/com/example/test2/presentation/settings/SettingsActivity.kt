bf77bab86f5c37250bbb60de77f06c5a800e0896
kataevrsc <kataevrsc@bitbucket.org>
2021-10-18 19:50:01 +0300

    change settings activity: - model architecture to MVVM - add result panel layout

--- app/src/main/java/com/example/test2/presentation/settings/SettingsActivity.kt
+++ app/src/main/java/com/example/test2/presentation/settings/SettingsActivity.kt
@@ -5,27 +5,30 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.activity.viewModels
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.test2.R
+import com.example.test2.databinding.SettingsActivityBinding
 
 
 class SettingsActivity : BaseActivity() {
 
-    companion object {
-        const val SETTINGS_RESULT_KEY = "SETTINGS_RESULT_KEY"
-    }
+    private val viewBinding by viewBinding(SettingsActivityBinding::bind)
+    private val viewModel: SettingsViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val data = intent.getIntExtra(SETTINGS_RESULT_KEY, -1)
-        Toast.makeText(this, "data is $data", Toast.LENGTH_SHORT).show()
+
         setContentView(R.layout.settings_activity)
 
-
-        val back = findViewById<ImageView>(R.id.settings_back)
-        back.setOnClickListener {
-            setResult(RESULT_OK, Intent().putExtra(SETTINGS_RESULT_KEY,"Hello!"))
+        viewBinding.settingsBack.setOnClickListener {
             finish()
+        }
+
+        viewBinding.resultPanel.setOnClickListener {
+            //viewModel.onResultPanelClick()
+            Toast.makeText(this, "Click", Toast.LENGTH_SHORT).show()
         }
     }
 }
---
+++ app/src/main/java/com/example/test2/presentation/settings/SettingsViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.test2.presentation.settings
+
+import androidx.lifecycle.ViewModel
+
+class SettingsViewModel: ViewModel() {
+
+    fun onResultPanelClick() {
+        TODO("Not yet implemented")
+    }
+
+
+}
